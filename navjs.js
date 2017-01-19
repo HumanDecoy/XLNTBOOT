e8ee686bdee1317675fb748a36ae4531cd815254
Oscar Gren <Oscar Gren>
2017-01-19 14:36:08 +0100

    Fulhackaren

--- navjs.js
+++ navjs.js
@@ -1,16 +1,27 @@
 // Tar ut elementet så att det är clickbart och tar bort menyn på scroll 
 var menuClick = document.getElementsByClassName("navbar-toggle");
+var btnNo= document.getElementById("noScrollbtn");
 // Scroll Funktionen // 
+
+btnNo.addEventListener("click", addClass);
+
+function addClass(){
+  btnNo.classList.add("noGo")
+  setTimeout(function(){btnNo.classList.remove("noGo");},130);
+
+}
+
 
 $('a[href*="#"]:not([href="#"])').click(function() {
   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
     var target = $(this.hash);
+    var pointer = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
     if (target.length) {
       $('html, body').animate({
         scrollTop: target.offset().top
       }, 1000);
-      if (target.length==13){
+      if (btnNo.classList == "btn-page-1 btn-group-justified noGo"){
         return false;
       }
       else{
