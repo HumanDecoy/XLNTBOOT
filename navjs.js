501cb7e4cf92cb9abdef855fac333374eda771bf
Eddie Jostell <eddie.jostell@gmail.com>
2017-01-19 13:38:53 +0100

    team.html start to live

--- navjs.js
+++ navjs.js
@@ -14,3 +14,17 @@ $('a[href*="#"]:not([href="#"])').click(function() {
   }
 });
 // Slut Scroll funktionen//
+
+
+jQuery(function ($) {
+    $('.carousel').carousel();
+    var caption = $('div.item:nth-child(1) .carousel-caption');
+    $('.new-caption-area').html(caption.html());
+    caption.css('display', 'none');
+
+    $(".carousel").on('slide.bs.carousel', function (evt) {
+        var caption = $('div.item:nth-child(' + ($(evt.relatedTarget).index() + 1) + ') .carousel-caption');
+        $('.new-caption-area').html(caption.html());
+        caption.css('display', 'none');
+    });
+});
